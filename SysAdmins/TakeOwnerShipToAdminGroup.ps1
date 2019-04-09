d000f6a92c0e243ba83bdeefacc5a4bd43dd63f3
JEREMYNO <jaroslav.mraz@allycom.sk>
2019-04-09 13:07:18 +0200

    Create TakeOwnerShipToAdminGroup.ps1
    First revision. Easy script to take ownership of selected folder and add it to Local Admin group on Windows PC or Server. Using powershell

---
+++ SysAdmins/TakeOwnerShipToAdminGroup.ps1
@@ -0,0 +1,9 @@
+﻿# Simple script to change ownership of folder and files inside to Admin group.
+#
+# Usage: ./TakeOwnerShipToAdminGroup.ps1 -dir "DIRECTORY TO CHANGE RIGHTS"
+#
+# v.1 Created by Jaroslav Mráz https://github.com/JEREYMNO
+
+ param([string] $dir)
+ 
+ takeown /f $dir /a /r /d Y
